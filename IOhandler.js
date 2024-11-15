be0a98e7d418856846d24ae9059146fbc66ed1b8
sooah1219 <gcho14@my.bcit.ca>
2024-11-14 19:18:02 -0800

    first commit

---
+++ IOhandler.js
@@ -0,0 +1,80 @@
+const fs = require("fs");
+const PNG = require("pngjs").PNG;
+const path = require("path");
+const yauzl = require('yauzl-promise'),
+  { pipeline } = require('stream/promises');
+
+/**
+ * Description: decompress file from given pathIn, write to given pathOut
+ *
+ * @param {string} pathIn
+ * @param {string} pathOut
+ * @return {promise}
+ */
+
+
+async function unzip(pathIn, pathOut) {
+  const zip = await yauzl.open(pathIn);
+  try {
+    await fs.promises.mkdir(pathOut, { recursive: true }); // recursive:true = 존재하지않을때 만들어주는것!
+    for await (const entry of zip) {
+      if (entry.filename.endsWith('/')) {
+        await fs.promises.mkdir(path.join(pathOut, entry.filename));
+      } else {
+        const readStream = await entry.openReadStream();
+        const writeStream = fs.createWriteStream(
+          path.join(pathOut, entry.filename)
+        );
+        await pipeline(readStream, writeStream);
+      }
+    }
+  } finally {
+    await zip.close();
+  }
+}
+
+/**
+ * Description: read all the png files from given directory and return Promise containing array of each png file path
+ *
+ * @param {string} path
+ * @return {promise}
+ */
+const readDir = (dir) => { };
+
+/**
+ * Description: Read in png file by given pathIn,
+ * convert to grayscale and write to given pathOut
+ *
+ * @param {string} filePath
+ * @param {string} pathProcessed
+ * @return {promise}
+ */
+const grayScale = (pathIn, pathOut) => {
+  fs.createReadStream(path.join(pathIn, "in.png"))
+    .pipe(
+      new PNG({
+        filterType: 4,
+      })
+    )
+    .on("parsed", function () {
+      for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+          var idx = (this.width * y + x) << 2;
+
+          const gray = this.data[idx] + this.data[idx + 1] + this.data[idx + 2];
+          // invert color
+          this.data[idx] = gray;
+          this.data[idx + 1] = gray;
+          this.data[idx + 2] = gray;
+
+        }
+      }
+      this.pack().pipe(fs.createWriteStream(path.join(pathOut, "out.png")));
+    });
+};
+
+module.exports = {
+  unzip,
+  readDir,
+  grayScale,
+};
---
+++ main.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+const IOhandler = require("./IOhandler");
+const zipFilePath = path.join(__dirname, "myfile.zip");
+const pathUnzipped = path.join(__dirname, "unzipped");
+const pathProcessed = path.join(__dirname, "grayscaled");
+
+// IOhandler.unzip(zipFilePath, pathUnzipped);
+IOhandler.grayScale(pathUnzipped, pathProcessed);
